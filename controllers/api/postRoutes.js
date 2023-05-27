a3fab07a8df93cd8545a05e7c2c847f5ad6de117
tbohn2 <tanner.bohn@yahoo.com>
2023-05-26 22:33:40 -0700

    Made post route for new posts

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
