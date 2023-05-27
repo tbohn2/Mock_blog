972eab45d8a2e6878a8a7049acfd47bfa2fb74c1
tbohn2 <tanner.bohn@yahoo.com>
2023-05-26 19:57:46 -0700

    Created seeds index.js

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const posts = await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  console.log(users);
+  console.log(posts);
+
+  process.exit(0);
+};
+
+seedDatabase();
