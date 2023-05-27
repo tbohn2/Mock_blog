551a2dcc5ffaf3128bd2f8c86ee6aa17abe03837
tbohn2 <tanner.bohn@yahoo.com>
2023-05-26 22:40:58 -0700

    Added route to update posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -13,4 +13,22 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.update(
+            {
+                title: req.body.title,
+                comment: req.body.comment
+            },
+            {
+                where: { id: req.params.id }
+            }
+        );
+
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
