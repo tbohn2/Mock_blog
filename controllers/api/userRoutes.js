53cd9eb48d8f3588869ea0e1893d7193896a9c6f
tbohn2 <tanner.bohn@yahoo.com>
2023-05-26 22:51:09 -0700

    Added post route for the creation of newUser

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            ...req.body
+        });
+
+        res.status(200).json(newUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
