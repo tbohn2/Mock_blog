40a185e0ae4c022862c4b6bbae350ea5ef79b12d
tbohn2 <tanner.bohn@yahoo.com>
2023-05-27 20:48:55 -0700

    Included script tag

---
+++ public/js/homepage.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+document.querySelector('#homebtn').addEventListener('click', loadHome);
+document.querySelector('#dashbtn').addEventListener('click', loadDashboard);
+document.querySelector('#logbtn').addEventListener('click', loadLogin);
+document.querySelector('.post').addEventListener('click', loadPost);
