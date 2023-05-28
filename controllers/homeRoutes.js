7ad3e511592fc1d6b21dc2e67448683f0e643acb
tbohn2 <tanner.bohn@yahoo.com>
2023-05-27 20:30:01 -0700

    Added handbars files to render on homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,13 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-    res.json(postData)
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+
   } catch (err) {
     res.status(500).json(err);
   }
