6979ea36705de66bdd8c7e6dcf51c3efac46dbca
tbohn2 <tanner.bohn@yahoo.com>
2023-05-27 21:58:06 -0700

    Fixed layout to display posts on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,23 +9,23 @@ router.get('/', async (req, res) => {
           model: User,
           attributes: ['username']
         },
-        {
-          model: Comments,
-          attributes: ['comment'],
-          include: [{
-            model: User,
-            attributes: ['username']
-          }]
-        },
+        // {
+        //   model: Comments,
+        //   attributes: ['comment'],
+        //   include: [{
+        //     model: User,
+        //     attributes: ['username']
+        //   }]
+        // },
       ],
     });
-    res.json(postData)
-    // const posts = postData.map((post) => post.get({ plain: true }));
 
-    // res.render('homepage', {
-    //   posts,
-    //   logged_in: req.session.logged_in
-    // });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
 
   } catch (err) {
     res.status(500).json(err);
