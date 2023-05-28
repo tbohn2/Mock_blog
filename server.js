650cda9f2c7901faa23c555ef32247f228d50102
tbohn2 <tanner.bohn@yahoo.com>
2023-05-27 20:29:07 -0700

    Uncommented handlebar references

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
@@ -30,8 +30,8 @@ const sess = {
 
 app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
