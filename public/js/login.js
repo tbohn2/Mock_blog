f01bb9231d11443a5be6bd6ee6361aa02ba195aa
tbohn2 <tanner.bohn@yahoo.com>
2023-05-28 19:30:19 -0700

    Created function to log in user

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginUser = async (event) => {
+    event.preventDefault();
+    const username = document.querySelector('#usernameInput').value.trim();
+    const password = document.querySelector('#passwordInput').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+
+document.querySelector('#loginbtn').addEventListener('click', loginUser);
+// document.querySelector('#signupbtn').addEventListener('click', createNewUser);
