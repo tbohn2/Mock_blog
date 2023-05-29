d732e177fbb1d220aa06c836e1d23b5d4085ddee
tbohn2 <tanner.bohn@yahoo.com>
2023-05-28 18:42:55 -0700

    Created routes to create User and login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,7 +7,46 @@ router.post('/', async (req, res) => {
             ...req.body
         });
 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+
         res.status(200).json(newUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password!' });
+            return;
+        }
+
+        const correctPass = userData.checkPassword(req.body.password);
+
+        if (!correctPass) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'Login successful!' });
+        });
+
     } catch (err) {
         res.status(400).json(err);
     }
